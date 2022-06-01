5f192cb63fef05f55e6c9e62376186961c731db6
john Doe <sataward666@gmail.com>
2022-06-01 18:01:22 +0200

    je narrive pas a changer la couleur

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,18 @@
 
-const clikAvatar = document.querySelector(".toto");
+/*const clikAvatar = document.querySelector(".toto");
 
 clikAvatar.addEventListener("click", function(){
     clikAvatar.src = ("./image/avatar-bis.png");
-})
+});*/
+
+const changeColor = function () {
+
+	const elementsToChange = document.getElementsById('myName');
+	
+	for ( let i = 0; i < elementsToChange.length; i++ ) {
+	
+		elementsToChange[i].classList.add('.change');
+	
+	}
+
+};
